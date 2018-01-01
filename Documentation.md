d5f274d1f79dec9fd57777dce21e06ac1da04f85
AleksPeneva <alexemilpeneva@abv.bg>
2018-01-01 21:36:12 +0200

    Added documentation + removed unnecessary comment

---
+++ Documentation.md
@@ -0,0 +1,11 @@
+Team name: Reptiles
+
+Team members: AleksPeneva, PetkoTihomirov
+
+Project purpose: Creating a simple console game version of the popular game Snake in order to practice OOP, teamwork and using Git
+
+Class diagram: ![Image of class diagram](https://github.com/ppetkovv/TeamProjects/blob/master/ClassDiagram.png)
+
+URL Git repository: https://github.com/ppetkovv/TeamProjects
+
+Used: Visual Studio 2017, GitHub, SourceTree
--- Snake/Common/Constants.cs
+++ Snake/Common/Constants.cs
@@ -5,7 +5,7 @@
         public const string GameOver = "Game over!!!";
         public const string InvalidCommand = "Invalid command!";
         public const string InvalidGameMode = "Please enter valid game mode: ";
-        public const string InvalidApplesState = "Cannot eat more than one apple!"; /// WHEN COULD WE ENCOUNTER THIS?
+        public const string InvalidApplesState = "Cannot eat more than one apple!";
         public const string GameLevel = "LEVEL";
         public const string GameStartPreparation = "The game will start in: ";
         public const string GameMode = @"Please choose from one of the game modes:
